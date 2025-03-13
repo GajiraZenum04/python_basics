f8a7465359b454e617d037121b025ec220d11d0a
haruna_hannah <harunahannah04@gmail.com>
2025-03-12 23:38:13 -0600

    003_numeric_data.py file created

--- 002_string.py
+++ 002_string.py
@@ -1,22 +1,22 @@
-my_sentence: "Python is fun and powerful!"
+my_sentence ="Python is fun and powerful"
 print(len(my_sentence))
 print(my_sentence[0])
-print(my_sentence[9:])
-lowercase_sentence = my_sentence.lower()
-print(lowercase_sentence)
-print(my_sentence.count(o))
-print(my_sentence[9:11])
-my_sentence = my_sentence.replace('powerful', 'amazing')
-print(my_sentence)
-
-new_message: "Learning python is exciting!"
-
-new_sentence = my_sentence + ', ' + new_message
+print(my_sentence[8:22])
+print(my_sentence.lower())
+print(my_sentence.count("O"))
+print(my_sentence.find("fun"))
+new_sentence = my_sentence.replace("Powerful", "Amazing")
 print(new_sentence)
-new_sentence = '{}, {}'.format(my_sentence, new_message)
-print(new_sentence)
-new_sentence =f'{my_sentence}, {new_message}'
-print(new_sentence)
+new_message = "Learning Python is exciting"
+sentence =my_sentence + ', ' + new_message
+print(sentence)
+sentence = '{}, {}'.format(my_sentence, new_sentence)
+print(sentence)
+sentence = f'{my_sentence}, {new_sentence}'
+print(sentence)
 print(dir(my_sentence))
-print(help(str))
+print(help(str.replace))
+
+
+
 
---
+++ 003_numeric_data.py
@@ -0,0 +1,38 @@
+num_1 = 7
+num_2 = 2.25
+
+print(7 + 2.25)
+print(7 - 2.25)
+print(7 * 2.25)
+print(7 / 2.25)
+print(7 // 2.25)
+print(7 ** 2.25)
+print(7 % 2.25)
+
+print((10 + 3) * 2 ** 2 / 4 - 1)
+
+
+print(abs(-15))
+
+print(round(2.25, 1))
+print(round(2.25, 2))
+
+print(num_1 == num_2)
+print(num_1 != num_2)
+print(num_1 > num_2)
+print(num_1 < num_2)
+print(num_1 >= num_2)
+print(num_1 <= num_2)
+
+num_2 = int(num_2)
+print(int(num_2))
+
+num_1 = float(num_1)
+print(float(num_1))
+
+num_3 = "20"
+num_3 = int(num_3)
+print(int(num_3))
+
+
+
