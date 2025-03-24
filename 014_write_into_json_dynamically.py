d3ca350548e38a21c1db76caedc86d018ce8390b
haruna_hannah <harunahannah04@gmail.com>
2025-03-24 01:19:13 -0600

    014_write_into_json_dynamically.py file created

--- 014_write_into_json_dynamically.py
+++ 014_write_into_json_dynamically.py
@@ -1,15 +1,15 @@
 import json
-a = input("id")
-b = input("first_name")
-c = input("last_name")
-d = input("email")
-e = input("password")
+a = input("id:")
+b = input("first_name:")
+c = input("last_name:")
+d = input("email:")
+e = input("password:")
 
-user_dict = {"id": 1,
-             "first_name": "grace",
-             "last_name": "gajira",
-             "email": "gracegajira01@gmail.com",
-             "password": "grace@04"
+user_dict = {"id": a,
+             "first_name": b,
+             "last_name": c,
+             "email": d,
+             "password": e
              }
 
 with open("015_register.json", "w") as my_file:
