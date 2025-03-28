3745111cd92120a0c745f1b57d8d0e4a5d866d88
haruna_hannah <harunahannah04@gmail.com>
2025-03-28 16:32:36 -0600

    022_for_loop.py file created

--- 022_for_loop.py
+++ 022_for_loop.py
@@ -1,7 +1,8 @@
 start = int(input("starting number: "))
 end = int(input("ending number: "))
-for num in range(start, end + 1):
-    print("even number in range:")
+print("even number in range:")
+for num in range (start, end +1):
+
     if num % 2 == 0:
         print(num)
 
