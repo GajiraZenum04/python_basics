b2ec3b9ced4a2a468fc0e41b8f29c7b4d1d891e4
haruna_hannah <harunahannah04@gmail.com>
2025-03-31 16:00:21 -0600

    026_function.py file created

---
+++ 026_function.py
@@ -0,0 +1,15 @@
+def greet_users():
+    print("Hello, welcome to python programming!")
+
+greet_users()
+
+def add_numbers(a, b):
+    c= a + b
+    add_numbers(5, 7)
+    print(c)
+
+def multiply_numbers(a, b):
+    return a * b
+total = multiply_numbers(5, 7)
+print("Total:", total)
+
