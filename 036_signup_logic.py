b740731cf80fa408187a02baa1c916e4d78a5ae6
haruna_hannah <harunahannah04@gmail.com>
2025-04-11 09:58:21 +0100

    037_signup_backup.py file created

--- 036_signup_logic.py
+++ 036_signup_logic.py
@@ -7,18 +7,17 @@ with open("035_signup_data.json", "r")as file:
     c = input("lastname:")
     d = input("email:")
     e = input("password:")
-
-    data_dict = {
+data_dict = {
             "id": a,
             "firstname": b,
             "lastname": c,
             "email": d,
             "password": e
             }
-    my_list_dict.append(data_dict)
+my_list_dict.append(data_dict)
 
     with open("035_signup_data.json", "w")as file:
         json.dump(my_list_dict, file)
 
-    print("data successfully added to 035_signup_data.json")
+print("data successfully added to 035_signup_data.json")
 
--- 037_signup_backup.py
+++ 037_signup_backup.py
@@ -4,8 +4,8 @@ import pandas as pd
 with open("035_signup_data.json", "r")as file:
     data = json.load(file)
 
-    df = pd.DataFrame(data)
-    df.to_excel("038_first_exam_result.xlsx", index=False)
+df = pd.DataFrame(data)
+df.to_excel("038_first_exam_result.xlsx", index=False)
 
-    print("Backup created as Excel successfully.")
+print("Backup created as Excel successfully.")
 
